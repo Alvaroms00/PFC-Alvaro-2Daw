3fdbf45bbb6c6c80f6feca30b20fe4bd2d1fc64e
Alvaroms00 <alvaro030400@gmail.com>
2021-10-08 17:02:18 +0200

    V2.0

--- php/productos.php
+++ php/productos.php
@@ -5,7 +5,24 @@ $contrasena = "";
 $db=mysql_connect($servidor,$usuario,$contrasena);
 mysql_select_db("ferreteria",$db);
 
-$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('6255808507' , 'Amoladora Makita', 'GA4530R', '80.23'); "
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('6255808507' , 'Amoladora Makita', 'GA4530R', '80.23'); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+$sql = "INSERT INTO productos(id, nombre, modelo, precio) VALUES ('', '', '', ''); ";
+
 
 $sql = $insert.$values;
 ?>
