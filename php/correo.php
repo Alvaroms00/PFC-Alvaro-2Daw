cfb3e7242f8309cd2abf29ff6cfac24fd1538f66
Alvaroms00 <alvaro030400@gmail.com>
2021-11-08 18:11:19 +0100

    V10.2

--- php/correo.php
+++ php/correo.php
@@ -9,13 +9,22 @@ if (isset($_POST['enviar'])) {
         $asunto = $_POST['asunto'];
         $mensaje = $_POST['mensaje'];
 
-        $header = "From: $email" . "\r\n";
-        $header = "Reply-To: alvaro030400@gmail.com" . "\r\n";
-        $header = "X-Mailer: PHP/" . phpversion();
-        $mail = @mail($email, $asunto, $mensaje, $header);
+        ini_set("mail.log", "/tmp/mail.log");
+        ini_set("mail.add_x_header", TRUE);
+
+        $to = 'alvaro030400@gmail.com';
+        $subject = 'Email Subject';
+        $message = $_POST['mensaje'];
+        $headers = implode("\r\n", [
+            'From: alvaro030400@gmail.com',
+            'Reply-To: alvaro030400@gmail.com',
+            'X-Mailer: PHP/', PHP_VERSION]);
+
+            $result = mail($to, $subject, $message, $headers);
     }
-    if ($mail) {
-        echo "Enviado con exito.";
+    if ($result) {
+        echo "Enviado con exito.";+
+        header('Location: index.html', true, 303);
     }else{
         echo "Ha habido un error";
     }
